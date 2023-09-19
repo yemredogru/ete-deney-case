c094ec8a4ab33ec6d50ff4c1ca8340454bb35b42
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2023-09-19 18:50:47 +0300

    left sidebar menu added with items and routes

---
+++ frontend/src/components/common/SidebarData.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+
+
+
+export const SidebarData = [
+  {
+    title: "Home Page",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+ 
+  },
+  {
+    title: "Companies",
+    path: "/company",
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+ 
+   
+  },
+  {
+    title: "Products",
+    path: "/products",
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  }
+];
